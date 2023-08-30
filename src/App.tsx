492687f29e3465013456132d257247877a76bc33
Sara Baqla <missatrox44@gmail.com>
2023-08-30 18:10:28 -0500

    pulling in products data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      .then(data => setProducts(data.products))
+      .catch(error => console.error("Error fetching products:", error));
+    console.log("products:", products)
+  }, []);
+
   return (
     <>
       <select className="w-[600px] h-[60px] border border-[#1AA2B0] rounded-[6px] px-3 mt-[285px]">

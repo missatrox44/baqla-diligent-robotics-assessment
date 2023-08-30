f6f6df0aeab868ae7247094963b65fcaafe423ac
Sara Baqla <missatrox44@gmail.com>
2023-08-30 18:54:55 -0500

    updated card styles to match figma

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -2,23 +2,29 @@ import React from 'react';
 
 const ProductDetails: React.FC = () => {
   return (
-
     <div className="grid grid-cols-2">
-      <div className="">
-        <img src="https://i.dummyjson.com/data/products/1/thumbnail.jpg" alt="iPhone 9" />
+      <div>
+        <img
+          src="https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+          alt="iPhone 9"
+          className="border-1 border-[#1AA2B0] rounded-lg h-36"
+        />
       </div>
 
       <div className="bg-[#151420]">
-        <div className="flex justify-between">
-          <p className="text-white text-18px font-bold">iPhone9</p>
-          <p className="text-white font-bold ">smartphones</p>
+        <div className="flex justify-between items-center">
+          <p className="text-white text-xl font-bold">iPhone 9</p>
+          <span className="text-white font-bold text-xs bg-[#1AA2B0] rounded-full px-2 py-1">
+            smartphones
+          </span>
         </div>
-        <p className="text-left">Apple</p>
-        <p className="text-left text-white font-bold">$549.00</p>
-        <p className="text-left text-white">An apple mobile which is nothing like apple</p>
+        <p className="text-left leading-10 text-sm">Apple</p>
+        <p className="text-left text-white text-3xl font-bold">$549.00</p>
+        <p className="text-left text-white leading-10 text-sm">An apple mobile which is nothing like apple</p>
       </div>
     </div>
   );
 }
+
 
 export default ProductDetails;

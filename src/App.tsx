e05eac1e1c8561c320443ade8384e51183ffcdec
Sara Baqla <missatrox44@gmail.com>
2023-08-30 18:13:22 -0500

    map products to options and define Product type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+type Product = {
+  id: string;
+  title: string;
+};
+
 const App: React.FC = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch('https://dummyjson.com/products')
@@ -16,10 +21,9 @@ const App: React.FC = () => {
     <>
       <select className="w-[600px] h-[60px] border border-[#1AA2B0] rounded-[6px] px-3 mt-[285px]">
         <option value="">Select a product</option>
-        <option value="Option 1">Option 1</option>
-        <option value="Option 2">Option 2</option>
-        <option value="Option 3">Option 3</option>
-        <option value="Option 4">Option 4</option>
+        {products.map(product => (
+          <option key={product.id} value={product.id}>{product.title}</option>
+        ))}
       </select>
       <p className="mt-[200px]">No product selected</p>
     </>

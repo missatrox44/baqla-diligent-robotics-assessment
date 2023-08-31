c2f0d63e664af3ae25db282a7631a7d65e24ec15
Sara Baqla <missatrox44@gmail.com>
2023-08-30 21:12:06 -0500

    added style but still has typescript errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import ProductDetails from './components/ProductDetails';
 import { useSpring, animated } from 'react-spring';
-
+import Select, { components, DropdownIndicatorProps, Props as SelectProps } from 'react-select';
 
 interface Product {
   id: number;
@@ -13,6 +13,65 @@ interface Product {
   price: number;
   description: string;
 }
+
+interface ProductOption {
+  value: number;
+  label: string;
+}
+
+// Custom Dropdown Indicator
+const CustomDropdownIndicator = (props: DropdownIndicatorProps) => {
+  return (
+    components.DropdownIndicator && (
+      <components.DropdownIndicator {...props}>
+        <div style={{ borderTop: '8px solid #1AA2B0', borderLeft: '8px solid transparent', borderRight: '8px solid transparent', height: 0, width: '16px' }}></div>
+      </components.DropdownIndicator>
+    )
+  );
+};
+
+const customStyles = {
+  control: (base: object) => ({
+    ...base,
+    backgroundColor: '#1E1D2D',
+    borderColor: '#1AA2B0',
+    borderRadius: '6px',
+    height: '60px',
+    padding: '5px',
+    color: '#1AA2B0',
+    width: '600px'
+  }),
+  option: (base: unknown, state: object) => ({
+    ...base,
+    backgroundColor: state.isSelected || state.isFocused ? '#1AA2B0' : '#1E1D2D',
+    color: state.isSelected || state.isFocused ? '#1E1D2D' : '#1AA2B0',
+    fontWeight: 'bold',
+    lineHeight: '30px',
+}),
+
+  singleValue: (base: object) => ({
+    ...base,
+    color: '#1AA2B0',
+  }),
+
+  menu: (base: object) => ({
+    ...base,
+    borderColor: '#1AA2B0',
+    borderRadius: '6px',
+    borderWidth: '1px',
+    boxShadow: 'none',
+    marginTop: '5px',
+  }),
+  menuList: (base: object) => ({
+    ...base,
+    overflowY: 'auto',
+    padding: 0, 
+    '&::-webkit-scrollbar': {
+      width: 0,
+      background: 'transparent',
+    },
+  }),
+};
 
 const App: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -40,10 +99,8 @@ const App: React.FC = () => {
     transform: 'scale(0.8)',
   }));
 
-
-  const handleProductChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = parseInt(event.target.value, 10);
-    const product = products.find(p => p.id === value);
+  const handleSelectProductChange = (selectedOption: ProductOption | null) => {
+    const product = products.find(p => p.id === selectedOption.value);
     setSelectedProduct(product || null);
 
     if (product) {
@@ -65,23 +122,20 @@ const App: React.FC = () => {
 
   return (
     <>
-      <select
-        style={{ backgroundColor: "#1E1D2D" }}
-        className="w-[600px] h-[60px] border border-[#1AA2B0] rounded-[6px] px-3"
-        onChange={handleProductChange}
-      >
-        <option value="">Select a product</option>
-        {products.map(product => (
-          <option key={product.id} value={product.id}>{product.title}</option>
-        ))}
-      </select>
+      <Select 
+        styles={customStyles}
+        components={{ DropdownIndicator: CustomDropdownIndicator, IndicatorSeparator: () => null }}
+        options={products.map(product => ({ value: product.id, label: product.title }))}
+        onChange={handleSelectProductChange}
+        placeholder="Select a product"
+      />
 
       {selectedProduct ? (
         <animated.div key={selectedProduct.id} style={detailsAnimation}>
           <ProductDetails product={selectedProduct} />
         </animated.div>
       ) : (
-        <animated.p style={noProductAnimation} className="mt-[200px] text-center">
+        <animated.p style={noProductAnimation} className="mt-[200px] text-center text-xl">
           No product selected
         </animated.p>
       )}

a249ea9ce2c4412e8787dde0cf482c49ca40ab2f
Sara Baqla <missatrox44@gmail.com>
2023-08-30 20:30:04 -0500

    installed react-spring and added animations to ProductDetails card when selected

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import ProductDetails from './components/ProductDetails';
+import { useSpring, animated } from 'react-spring';
 
 
 interface Product {
@@ -28,13 +29,39 @@ const App: React.FC = () => {
       .catch(error => console.error("Error fetching products:", error));
   }, []);
 
+  // Initial animation states
+  const [detailsAnimation, setDetailsAnimation] = useSpring(() => ({
+    opacity: 0,
+    transform: 'translateY(30px)',
+  }));
+
+  const [noProductAnimation, setNoProductAnimation] = useSpring(() => ({
+    opacity: 1,
+    transform: 'scale(0.8)',
+  }));
+
+
   const handleProductChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = parseInt(event.target.value, 10);
     const product = products.find(p => p.id === value);
-    // console.log("Selected Product:", product);
     setSelectedProduct(product || null);
-  };
 
+    if (product) {
+      // animate Product Details card
+      setDetailsAnimation({
+        opacity: 1,
+        transform: 'translateY(0px)',
+        from: { opacity: 0, transform: 'translateY(30px)' },
+      });
+    } else {
+      // add movement to "no product selected" text
+      setNoProductAnimation({
+        opacity: 1,
+        transform: 'scale(1)',
+        from: { opacity: 0, transform: 'scale(0.8)' },
+      });
+    }
+  };
 
   return (
     <>
@@ -49,8 +76,15 @@ const App: React.FC = () => {
         ))}
       </select>
 
-      {selectedProduct ? <ProductDetails product={selectedProduct} /> : <p className="mt-[200px] text-center">No product selected</p>}
-
+      {selectedProduct ? (
+        <animated.div key={selectedProduct.id} style={detailsAnimation}>
+          <ProductDetails product={selectedProduct} />
+        </animated.div>
+      ) : (
+        <animated.p style={noProductAnimation} className="mt-[200px] text-center">
+          No product selected
+        </animated.p>
+      )}
     </>
   );
 }
